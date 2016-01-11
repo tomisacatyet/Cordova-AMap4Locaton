8da4cca83048eeddae495931fa029e34dca07c83
tomisacatyet <akso@Macintosh.local>
2016-01-11 16:39:49 +0800

    修正

--- www/amap4location.js
+++ www/amap4location.js
@@ -4,12 +4,12 @@ var exec = require('cordova/exec');
 
 module.exports = {
     init: function (successCallback, errorCallback) {
-        cordova.exec(successCallback, errorCallback, "AMapLocation4Plugin", "initConfig", []);
+        cordova.exec(successCallback, errorCallback, "AMap4LocationPlugin", "initConfig", []);
     },
     location: function (successCallback, errorCallback) {
-        cordova.exec(successCallback, errorCallback, "AMapLocation4Plugin", "location", []);
+        cordova.exec(successCallback, errorCallback, "AMap4Location4Plugin", "location", []);
     },
     stop: function (successCallback, errorCallback) {
-        cordova.exec(successCallback, errorCallback, "AMapLocation4Plugin", "stop", []);
+        cordova.exec(successCallback, errorCallback, "AMap4Location4Plugin", "stop", []);
     }
 };
